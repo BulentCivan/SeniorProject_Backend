db732332cf85c3fc9df63671a3bca40dd0d6734e
BulentCivan <bulentcivan7@gmail.com>
2024-05-18 10:37:18 +0300

    test

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c3016e37e90a5d4c13552ba9fc307da43332de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3d2312167d79285670c56515357cad5cd34527")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
